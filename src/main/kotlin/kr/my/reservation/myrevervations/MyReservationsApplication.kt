b6d25c13c1101a0eb5f84f573cc3e0ede4332a99
devbloo <zpswl45@naver.com>
2023-05-30 22:43:34 +0900

    [BE-001] init: project

---
+++ src/main/kotlin/kr/my/reservation/myrevervations/MyReservationsApplication.kt
@@ -0,0 +1,11 @@
+package kr.my.reservation.myrevervations
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyReservationsApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyReservationsApplication>(*args)
+}
---
+++ src/test/kotlin/kr/my/reservation/myrevervations/MyReservationsApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.my.reservation.myrevervations
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyReservationsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
